54f0bd98dc6da18333de3774a7bcf57d66bdda98
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-17 12:01:32 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,5 +1,5 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+m = int(input("Enter a number: "));
+if (m % 2) == 0:
+   print("this number is  Even")
 else:
-   print("{0} is Odd".format(num))
+   print("this number is Odd")
