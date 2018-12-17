cac6d526f4ee1980cbc924795aa6fa039ec40c11
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-17 15:26:55 +0530

    Update largest number.py

--- largest number.py
+++ largest number.py
@@ -2,9 +2,9 @@ num1 = 10
 num2 = 14
 num3 = 12
 if (num1 >= num2) and (num1 >= num3):
-largest = num1
+ largest = num1
 ellif (num2 >= num1) and (num2 >= num3):
  largest = num2
- else:
+else:
  largest = num3
- print ("the largest number between",num1,",",num2,"and",num3,"is",largest)
+print ("the largest number between",num1,",",num2,"and",num3,"is",largest)
