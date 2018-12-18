f3064613ba2bf7ff1ea0f343c203647c671f224f
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-18 15:24:02 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,10 +3,10 @@ num = 7
 factorial= 1
 
 if nnum< 0
-  print("sorry factorial does not exit for negative number")
+    print("sorry factorial does not exit for negative number")
 elif num == 0:
-   print("the factorial of 0 is 1")
+    print("the factorial of 0 is 1")
 else:
-   for i in range(1,num + 1):
-       factorial = factorial*in
-   print("the factorial of",num,"is",factorial)
+    for i in range(1,num + 1):
+        factorial = factorial*in
+    print("the factorial of",num,"is",factorial)
