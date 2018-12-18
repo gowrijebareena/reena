d531976c9d658fc8de0508a049496d3c6a2b956b
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-18 14:30:03 +0530

    Update even num.py

--- even num.py
+++ even num.py
@@ -2,4 +2,4 @@ lower_limit=int(input("enter the lower limit:"))
 upper_limit=int(input("enter the upper limit:"))
 for i in range (lower,upper+1):
     if(i%2!=0):
-       print (i)
+        print (i)
