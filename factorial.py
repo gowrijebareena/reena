ba87d3ed97186186592b9572a29108de61631bb8
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-18 15:22:22 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,12 @@
- if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
-       if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif:
-num == 0:
-   print("The factorial of 0 is 1")
+num = 7
+
+factorial= 1
+
+if nnum< 0
+  print("sorry factorial does not exit for negative number")
+elif num == 0:
+   print("the factorial of 0 is 1")
 else:
-   print("The factorial of",num,"is",recur_factorial(num))
+   for i in range(1,num + 1):
+       factorial = factorial*in
+   print("the factorial of",num,"is",factorial)
