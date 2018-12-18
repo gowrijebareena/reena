730d9a96c3ec60c74ae496ef2659d5bb54017b4f
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-18 14:54:16 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -4,8 +4,8 @@ while temp > 0:
    digit = temp % 10
    sum += digit ** 3
    temp 
-if num==sum:
-   print("num is an armstrong number")
+if num == sum:
+   print ("num is an armstrong number")
 else:
-   print("num,"is not an armstrong number)
+   print ("num,"is not an armstrong number)
    
