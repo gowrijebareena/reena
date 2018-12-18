8cb498df6ed7f33a2048c6cc0c3217414e2a1eb2
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-18 14:36:15 +0530

    Update 2 numbers .py

--- 2 numbers .py
+++ 2 numbers .py
@@ -2,4 +2,4 @@ lower=int(input("enter the lower limit for the range"))
 upper=int(input("enter the upper limit for the range"))
 for i in range(lower,upper+1):
     if(i%2!=0):
-      print (i)
+        print (i)
