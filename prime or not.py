c36cd42345dc738e0cba519161c5a8861195cf33
gowrijebareena <45750647+gowrijebareena@users.noreply.github.com>
2018-12-18 12:42:17 +0530

    Update prime or not.py

--- prime or not.py
+++ prime or not.py
@@ -1,10 +1,10 @@
 if num > 1
   for i in range(2,num):
     if (num % i) == 0:
-      print (num," is not prime number")
-      print (i,"time","num//i, "is", num)
-      break
+        print (num," is not prime number")
+        print (i,"time","num//i, "is", num)
+        break
     else:
-      print (num,"is a prime number")
+        print (num,"is a prime number")
  else:
-   print (num,"is not a prime number")
+     print (num,"is not a prime number")
